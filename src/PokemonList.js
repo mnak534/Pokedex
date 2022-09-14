4c500e8e6a15b933d0ab780005c7decbd7e07a6c
Moeka <monjiro1025@gmail.com>
2022-09-14 21:28:47 +1200

    commit

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -4,9 +4,6 @@ import {useContext} from 'react';
 import PokemonDetail from './PokemonDetail';
 
 function PokemonList() {
-    // const lists = [{id:"001", name: "Bulabsaur"}, 
-    // {id:"002", name: "Ivysaur"},
-    // {id: "003", name: "Venusaur"}]
 
     const {currentPokemon, onPokemonClick} = useContext(AppContext);
 
@@ -16,7 +13,6 @@ function PokemonList() {
             <ul>
                 {pokemonList.map((pokemon, index) => (
                     <li key = {pokemon.id} onClick={() => onPokemonClick(index)}> No.{pokemon.id} : {pokemon.name} </li>
-                    // <li key = {pokemon.id}> No.{pokemon.id} : {pokemon.name} </li>
                 ))}
             </ul>
         </div>
