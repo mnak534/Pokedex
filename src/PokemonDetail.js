af8df9af0264c13b8f80293a2831153a633ed1dd
Moeka <monjiro1025@gmail.com>
2022-09-15 01:02:56 +1200

    Fixed some bugs

--- src/PokemonDetail.js
+++ src/PokemonDetail.js
@@ -13,10 +13,3 @@ export default function PokemonDetail({ pokemon }) {
         </div>
     )
 }
-
-// function AboutMe({name, age}) {
-//     return (
-//         <p>My name is {name}, and I am {age} years old!</p>
-//     )
-// }
-
